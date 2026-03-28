56b7b4457c990402efc6951c97ddc4247f1a6689
Sourav Kumar <xsouravkumar357@gmail.com>
2026-03-28 18:31:43 +0530

    Movie Card component jsx file

--- react-movie-site/src/App.jsx
+++ react-movie-site/src/App.jsx
@@ -1,27 +1,17 @@
 
 
-
+import MovieCard from './components/MovieCard'
 import './App.css'
 
-function App() {
 
+function App() {
   return (
     <>
-      <div>
-        hello world
-      </div>
-    
-      <Text/>
+      <MovieCard movie={{ title: "Tim's Film", release_date: "2024" }} />
+      <MovieCard movie={{ title: "Sourav Movie", release_date: "2025" }} />
     </>
   )
 }
 
-function Text(params) {
-  return (
-    <div>
-      hello world from another function
-    </div>
-  )
-}
 
 export default App
