faee7db14125323da1c1684a6aad3a25dc387b85
Sourav Kumar <xsouravkumar357@gmail.com>
2026-03-28 18:42:18 +0530

    movie card complete

--- react-movie-site/src/App.jsx
+++ react-movie-site/src/App.jsx
@@ -1,14 +1,13 @@
 
 
-import MovieCard from './components/MovieCard'
 import './App.css'
 
 
 function App() {
+
   return (
     <>
-      <MovieCard movie={{ title: "Tim's Film", release_date: "2024" }} />
-      <MovieCard movie={{ title: "Sourav Movie", release_date: "2025" }} />
+      
     </>
   )
 }
